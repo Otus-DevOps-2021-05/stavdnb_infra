4bbb955e87606d48ab5268ad49f8673fefda1aba
Mikhail Kolesnichenko <m.kolesnichenko@devex.pro>
2021-07-14 20:34:38 +0300

    Bugfix2

--- VPN/setupvpn.sh
+++ VPN/setupvpn.sh
@@ -1,4 +1,3 @@
-cat <<EOF> setupvpn.sh
 #!/bin/bash
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.4.list
 echo "deb http://repo.pritunl.com/stable/apt xenial main" > /etc/apt/sources.list.d/pritunl.list
@@ -10,3 +9,5 @@ apt-get --assume-yes install pritunl mongodb-org
 systemctl start pritunl mongod
 systemctl enable pritunl mongod
 EOF
+
+#theend
